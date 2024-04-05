2cae24ae78291d4645d7022a51cc55f409295af4
ys <ysdeveloper01@gmail.com>
2024-04-05 23:40:50 +0800

    finished chapter 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -73,6 +73,8 @@ export async function fetchCardData() {
       customerCountPromise,
       invoiceStatusPromise,
     ]);
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
     const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
